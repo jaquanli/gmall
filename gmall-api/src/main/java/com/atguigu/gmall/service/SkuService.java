6ac7807b20cd4f0170da238504a834d82fec1f41
A <A@guigu.com>
2018-12-08 14:57:18 +0800

    增加根据id查询SkuInfo和根据spuId查询SkuInfo和SkuSaleAttrValue的接口

--- gmall-api/src/main/java/com/atguigu/gmall/service/SkuService.java
+++ gmall-api/src/main/java/com/atguigu/gmall/service/SkuService.java
@@ -8,4 +8,8 @@ public interface SkuService {
     List<SkuInfo> querySkuInfoListBySpuInfoId(String spuInfoId);
 
     void saveSku(SkuInfo skuInfo);
+
+    SkuInfo querySkuInfoById(String skuId);
+
+    List<SkuInfo> querySkuInfoAndSkuSaleSttrValueBySpuId(Long spuId);
 }
